8fa231d020efee5d1affa66856b58b75096fad63
Batuhan Dursun <frutbn@protonmail.com>
2023-11-08 15:48:20 +0300

    Add ColorExtensions.cs (ToColor method)

---
+++ Runtime/ColorExtensions.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class ColorExtensions
+{
+    public static Color ToColor(this string strColor)
+    {
+        var rgba = strColor.Substring(5, strColor.Length - 6).Split(", ");
+        var color = new Color(float.Parse(rgba[0]), float.Parse(rgba[1]), float.Parse(rgba[2]), float.Parse(rgba[3]));
+        return color;
+    }
+}
