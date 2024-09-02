e487ce1795f44b677e5e97398fefc8cc782be755
Batuhan Dursun <git@batuhandursun.com>
2024-09-02 23:09:57 +0300

    Update Vector3 extension

--- Runtime/Extensions/Extensions.Vector3.cs
+++ Runtime/Extensions/Extensions.Vector3.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static partial class Extensions
 {
-    public static Vector3 Add(this Vector3 v, Vector3 direction, float value) => v + direction * value;
+    public static Vector3 Add(this Vector3 v, Vector3 direction, float value) => v + direction.normalized * value;
 
     public static Vector3 AddX(this Vector3 v, float value) => v.Add(Vector3.right, value);
 
